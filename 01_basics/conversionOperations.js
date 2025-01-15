2b0d9cd7fe7b299bf093752d8bbdb2bb3f55acf5
onkargagare2001 <71821005+onkargagare2001@users.noreply.github.com>
2025-01-15 05:06:07 +0000

    conversion operations

---
+++ 01_basics/conversionOperations.js
@@ -0,0 +1,11 @@
+let a=undefined;
+
+//declare b;
+let b=Number(a);
+let c=Boolean(b);
+console.log(b);
+console.log([typeof a,typeof b,typeof c]);
+
+// typeof null is object
+// "23asa" =convert> Number() then it will show as NaN(Not a Number);
+
