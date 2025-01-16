9670cb7e9aac7d0045530be580ab4a00a2892953
onkargagare2001 <71821005+onkargagare2001@users.noreply.github.com>
2025-01-16 14:16:49 +0000

    comparison

---
+++ 01_basics/comparison.js
@@ -0,0 +1,9 @@
+// console.log(null == 0);
+
+// console.table([undefined<=0,undefined>0,(undefined<0)]);
+
+// console.log(undefined >= 0);
+
+// triple equal 
+
+console.log((2+1) === (2+1));
--- 01_basics/conversionOperations.js
+++ 01_basics/conversionOperations.js
@@ -9,3 +9,17 @@ console.log([typeof a,typeof b,typeof c]);
 // typeof null is object
 // "23asa" =convert> Number() then it will show as NaN(Not a Number);
 
+// let bc =(3+3-"1"+(2+2));
+// const number=3;
+
+// let dv=-number;
+
+// console.log(typeof(bc));
+// console.log("dv is",-dv);
+// console.log("type of dv is", typeof(dv));
+
+//console.log("The out ",true+);
+
+let gc=1;
+
+console.log(++gc);
