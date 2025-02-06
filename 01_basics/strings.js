d357b8b91b3658f6de23edcaff989d61ee860876
onkargagare2001 <71821005+onkargagare2001@users.noreply.github.com>
2025-02-06 18:22:49 +0000

    Strings

--- 01_basics/strings.js
+++ 01_basics/strings.js
@@ -1,3 +1,11 @@
 let brandName = new String('nokia');
 
-console.log('asasa',brandName);
+//console.log('asasa',brandName);
+
+//console.log(`my mobille is ${brandName}`);
+//console.log(brandName.charAt(1));
+//console.log(brandName.slice(-4,3));
+
+console.log(brandName.bold());
+console.log(brandName.blink());
+
