865911af22d3da072f64b2ec3a9521a330f0a55c
onkargagare2001 <71821005+onkargagare2001@users.noreply.github.com>
2025-02-15 04:20:02 +0000

    maths and nums

---
+++ 01_basics/maths.js
@@ -0,0 +1,10 @@
+const x=12.3;
+
+// console.log(Math.ceil(x));
+// console.log(Math.floor(x));
+// console.log(Math.round(x));
+// console.log(Math.random());
+
+let rand=Math.round(Math.random()*10);
+
+console.log(`${Math.random()} ${rand}`);
